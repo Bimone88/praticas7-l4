409612031bec650a98e9c86c344b70e74e54a2b3
Simone Manca <simone.manca.98@gmail.com>
2024-04-18 16:02:48 +0200

    EserciziGIT

---
+++ src/main/java/simonemanca/eserciziGIT/EserciziGitApplication.java
@@ -0,0 +1,13 @@
+package simonemanca.eserciziGIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EserciziGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EserciziGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/simonemanca/eserciziGIT/EserciziGitApplicationTests.java
@@ -0,0 +1,13 @@
+package simonemanca.eserciziGIT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EserciziGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
